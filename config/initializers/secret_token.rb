ba9ceb6434de11cac167431cd3f0a13ed50015ea
Kirill Nikitin <locke23rus@gmail.com>
2012-05-01 18:22:26 +0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estimatr::Application.config.secret_token = 'c9b31dd928714397fa09c1db6f78c6f27c88782dc16d11ae95700f544e6ffcc4b911c2056d4d31fd560f12cda83cf2e6ac4afc8df6ccec8af87f73fcc3d6a9bf'
